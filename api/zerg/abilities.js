1cc69696b7f4ae4600d2c10c4b5c861af2ec47bd
JamieVaughn <wxv6@case.edu>
2024-05-09 19:46:57 -0400

    remove object.assign, replace with spread

--- api/zerg/abilities.js
+++ api/zerg/abilities.js
@@ -2,7 +2,7 @@ import { Spells, Passives } from './data/abilities.js'
 
 export default (req, res) => {
     res.statusCode = 200
-    const Abilities = Object.assign({}, Spells, Passives)
+    const Abilities = { ...Spells, ...Passives}
     res.json(Abilities)
   }
   
